c02c70ed78dfd54c83370a75fa3063f123fe2485
Andrew <expandrew@gmail.com>
2017-05-24 15:42:55 -0700

    include more practice applications.

---
+++ Practice.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Practice
+{
+  static void Main()
+  {
+  Console.WriteLine("I love chocolate!");
+  Console.WriteLine("Coding is awesomesauce");
+  Console.WriteLine("I swear by my tattoo");
+  }
+}
---
+++ calculate.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Calculate
+{
+  static void Main()
+  {
+    Console.WriteLine("Enter a number:");
+    string firstString = Console.ReadLine();
+    int firstNumber = int.Parse(firstString);
+
+    Console.WriteLine("Enter another number:");
+    string secondString = Console.ReadLine();
+    int secondNumber = int.Parse(secondString);
+
+    Console.WriteLine("Please enter an operator symbol for the type of mathematical operation you would like to perform:");
+    string operatorSymbol = Console.ReadLine();
+    {
+      if (operatorSymbol == "+") {
+        int yourAnswer = firstNumber + secondNumber;
+        Console.WriteLine(yourAnswer);
+      }
+      else if (operatorSymbol == "-") {
+        int yourAnswer = firstNumber - secondNumber;
+        Console.WriteLine(yourAnswer);
+      }
+      else if (operatorSymbol == "*")  {
+        int yourAnswer = firstNumber * secondNumber;
+        Console.WriteLine(yourAnswer);
+      }
+      else if (operatorSymbol == "/") {
+        int yourAnswer = firstNumber / secondNumber;
+        Console.WriteLine(yourAnswer);
+      }
+      else if (operatorSymbol != "+" || operatorSymbol != "-" || operatorSymbol != "*" || operatorSymbol != "/") {
+        Console.WriteLine("Please enter a valid operator symbol!");
+      }
+    }
+  }
+}
